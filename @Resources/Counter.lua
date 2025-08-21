c9b48f2a818b083edca6cd7c57b87f21847cd731
VgonelV <mateuszbig2007@gmail.com>
2025-08-21 14:03:34 +0200

    level system

--- @Resources/Counter.lua
+++ @Resources/Counter.lua
@@ -1,15 +1,29 @@
 local time = 0
+local levelRequirement = 3600
+local level = 0
 
 function Initialize()
     time = tonumber(SKIN:GetVariable('Time')) or 0
-    SKIN:Bang('!SetOption MeterCounter Text "Time: ' .. time .. '"')
+    levelRequirement = tonumber(SKIN:GetVariable('LevelRequirement')) or 3600
+    level = tonumber(SKIN:GetVariable('Level')) or 0
+    SKIN:Bang('!SetOption MeterCounter Text "' .. time .. '\n/\n' .. levelRequirement .. '"')
 end
 
 function Update()
     time = time + 1
-    return time
+    if time >= levelRequirement then
+        time = 0
+        level = level + 1
+        levelRequirement = math.floor(levelRequirement * 1.2 * level)
+        SKIN:Bang('!SetOption MeterLevel Text "' .. level .. '" "Personal\\Level"')
+        SKIN:Bang('!UpdateMeter MeterLevel "Personal\\Level"')
+        SKIN:Bang('!Redraw "Personal\\Level"')
+    end
+    return time .. '\n/\n' .. levelRequirement
 end
 
 function SaveValue()
     SKIN:Bang('!WriteKeyValue', 'Variables', 'Time', time, '#@#Variables.inc')
+    SKIN:Bang('!WriteKeyValue', 'Variables', 'LevelRequirement', levelRequirement, '#@#Variables.inc')
+    SKIN:Bang('!WriteKeyValue', 'Variables', 'Level', level, '#@#Variables.inc')
 end
